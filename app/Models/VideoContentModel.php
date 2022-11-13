b216e240160197e93cb10f42950a7fd7943b8cc5
dancerking <dancerking306@gmail.com>
2022-11-13 22:02:44 +0100

    fixed json format for photo/video

--- app/Models/VideoContentModel.php
+++ app/Models/VideoContentModel.php
@@ -53,26 +53,23 @@ class VideoContentModel extends Model
 
     public function get_level1_video($host_id) {
         $db = \Config\Database::connect();
-        $query   = $db->query('SELECT video_contents.video_content_id, video_contents.video_content_channel, video_contents.video_content_code, video_contents.video_content_status, IF' . '(ISNULL(content_captions.content_caption)=1, ' . '""' . ', content_captions.content_caption) AS content_caption, IF' . '(ISNULL(content_captions.content_caption_lang)=1, ' . '""' . ', content_captions.content_caption_lang) AS content_caption_lang
-        FROM video_contents LEFT JOIN content_captions ON video_contents.video_content_id = content_captions.content_caption_connection_id AND content_captions.content_caption_type="2" AND content_captions.content_caption_host_id = ' . $host_id . '
-        WHERE video_contents.video_content_level = "1" AND  video_contents.video_content_host_id = ' . $host_id);
+        $query   = $db->query('SELECT video_content_id, video_content_channel, video_content_code, video_content_status
+        FROM video_contents
+        WHERE video_content_level = "1" AND  video_content_host_id = ' . $host_id);
         $results = $query->getResult();
         foreach($results as &$result) {
-            $content_caption = [
-                'content_caption' => $result->content_caption,
-                'content_caption_lang' => $result->content_caption_lang,
-            ];
-            $result->content_caption = $content_caption;
+            $content_caption_query = $db->query('SELECT IF' . '(ISNULL(content_caption)=1, ' . '""' . ', content_caption) AS content_caption, IF' . '(ISNULL(content_caption_lang)=1, ' . '""' . ', content_caption_lang) AS content_caption_lang FROM content_captions WHERE content_caption_connection_id = ' . $result->video_content_id . ' AND content_caption_host_id = ' . $host_id . ' AND content_caption_type = 2');
+            $content_caption_results = $content_caption_query->getResult();
+            $result->content_caption = $content_caption_results;
         }
         return $results;
     }
 
     public function get_level2_video($host_id) {
         $db = \Config\Database::connect();
-        $query   = $db->query('SELECT video_contents.video_content_id, video_contents.video_content_connection, video_contents.video_content_channel, video_contents.video_content_code, video_contents.video_content_status, IF' . '(ISNULL(content_captions.content_caption)=1, ' . '""' . ', content_captions.content_caption) AS content_caption, IF' . '(ISNULL(content_captions.content_caption_lang)=1, ' . '""' . ', content_captions.content_caption_lang) AS content_caption_lang
+        $query   = $db->query('SELECT video_content_id, video_content_connection, video_content_channel, video_content_code, video_content_status
         FROM video_contents
-        LEFT JOIN content_captions ON video_contents.video_content_id = content_captions.content_caption_connection_id AND content_captions.content_caption_type = "1" AND content_captions.content_caption_host_id = ' . $host_id . '
-        WHERE video_contents.video_content_level = "2" AND video_contents.video_content_host_id = ' . $host_id);
+        WHERE video_content_level = "2" AND video_content_host_id = ' . $host_id);
         $results = $query->getResult();
         foreach($results as &$result) {
             $query = $db->query('SELECT types_mapping.type_mapping_name
@@ -84,11 +81,9 @@ class VideoContentModel extends Model
                 array_push($type_mapping_names, $mapping_name->type_mapping_name);
             }
             $result->type_mapping_name = $type_mapping_names == null ? [] : $type_mapping_names;
-            $content_caption = [
-                'content_caption' => $result->content_caption,
-                'content_caption_lang' => $result->content_caption_lang,
-            ];
-            $result->content_caption = $content_caption;
+            $content_caption_query = $db->query('SELECT IF' . '(ISNULL(content_caption)=1, ' . '""' . ', content_caption) AS content_caption, IF' . '(ISNULL(content_caption_lang)=1, ' . '""' . ', content_caption_lang) AS content_caption_lang FROM content_captions WHERE content_caption_connection_id = ' . $result->video_content_id . ' AND content_caption_host_id = ' . $host_id . ' AND content_caption_type = 2');
+            $content_caption_results = $content_caption_query->getResult();
+            $result->content_caption = $content_caption_results;
         }
         return $results;
     }
