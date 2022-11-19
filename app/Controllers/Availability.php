72b24cf4433ddd1f91767fd396189c250c0894d8
dancerking <dancerking306@gmail.com>
2022-11-19 03:14:43 +0100

    updated availability endpoint

--- app/Controllers/Availability.php
+++ app/Controllers/Availability.php
@@ -29,7 +29,7 @@ class Availability extends APIBaseController
             'availabilityFrom'  => 'required',
             'availabilityTo'    => 'required'
         ];
-        if(!$this->validate($rules)) return $this->fail($this->validator->getErrors());
+        if(!$this->validate($rules)) return $this->notifyError('Input data format is incorrect', 'invalid_data', 'availability');
 
         /* Getting availability type from TypeAvailabilityModel */
         $type_availability_code = $this->request->getVar('l2_type_code'); // L2 type code
@@ -37,25 +37,18 @@ class Availability extends APIBaseController
         $to = $this->request->getVar('availabilityTo'); // To type_availability_day
 
         // check if date format
-        if(!$this->validateDate($from)) {
-            return $this->respond([
-                'Date format error' => 'from date format is incorrect'
-            ]);
-        }
-        if(!$this->validateDate($to)) {
-            return $this->respond([
-                'Date format error' => 'to date format is incorrect'
-            ]);
+        if(!$this->validateDate($from) || !$this->validateDate($to)) {
+            return $this->notifyError('Date format is incorrect', 'invalid_data', 'availability');
         }
         if(new DateTime($to) < new DateTime($from)) {
-            return $this->respond([
-                'Date Error' => 'To date should be larger than From date.'
-            ]);
+            return $this->notifyError('To date should larger than From date.', 'invalid_data', 'availability');
         }
         // getting availability data from model
         $availability_type = $type_availability_model->get_availability_types($host_id, $type_availability_code, $from, $to);
 
-        return $this->respond($availability_type == null ? [] : $availability_type, 200);
+        return $this->respond([
+            'availability_type' => $availability_type == null ? [] : $availability_type,
+        ]);
 	}
 
     /**
@@ -81,7 +74,7 @@ class Availability extends APIBaseController
             'type_availability_coa'  => 'required',
             'type_availability_cod'  => 'required',
         ];
-        if(!$this->validate($rules)) return $this->fail($this->validator->getErrors());
+        if(!$this->validate($rules)) return $this->notifyError('Input data format is incorrect', 'invalid_data', 'availability');
 
         /* Getting data from raw */
         $type_availability_id   = $this->request->getVar('type_availability_id');
@@ -93,35 +86,23 @@ class Availability extends APIBaseController
         $type_availability_cod  = $this->request->getVar('type_availability_cod');
 
         /* Format validation */
-        if(!$this->validateDate($type_availability_day)) {
-            return $this->respond([
-                'Date format error' => 'Date format is incorrect.'
-            ]);
-        }
         if(!ctype_digit((string)$type_availability_id)) {
-            return $this->respond([
-                'Int format error' => 'type_availability_id format is incorrect.'
-            ]);
+            return $this->notifyError('Type availability id format is incorrect', 'invalid_data', 'availability');
+        }
+        if(!$this->validateDate($type_availability_day)) {
+            return $this->notifyError('Date format is incorrect', 'invalid_data', 'availability');
         }
         if(!ctype_digit((string)$type_availability_qty)) {
-            return $this->respond([
-                'Int format error' => 'type_availability_qty format is incorrect.'
-            ]);
+            return $this->notifyError('Type availability qty format is incorrect', 'invalid_data', 'availability');
         }
         if(!ctype_digit((string)$type_availability_msa)) {
-            return $this->respond([
-                'Int format error' => 'type_availability_msa format is incorrect.'
-            ]);
+            return $this->notifyError('Type availability msa format is incorrect', 'invalid_data', 'availability');
         }
         if(!ctype_digit((string)$type_availability_coa)) {
-            return $this->respond([
-                'Int format error' => 'type_availability_coa format is incorrect.'
-            ]);
+            return $this->notifyError('Type availability coa format is incorrect', 'invalid_data', 'availability');
         }
         if(!ctype_digit((string)$type_availability_cod)) {
-            return $this->respond([
-                'Int format error' => 'type_availability_cod format is incorrect.'
-            ]);
+            return $this->notifyError('Type availability cod format is incorrect', 'invalid_data', 'availability');
         }
 
         /* Update data in DB */
@@ -135,12 +116,11 @@ class Availability extends APIBaseController
             'type_availability_cod'     => $type_availability_cod,
         ];
         if(!$type_availability_model->update($type_availability_id, $data)) {
-            return $this->respond([
-                'Failed message' => 'Failed update'
-            ]);
+           return $this->notifyError('Failed update', 'failed_update', 'availability');
         }
         return $this->respond([
-            'Success' => 'Successfully updated'
+            'id'        => $type_availability_id,
+            'Success'   => 'Successfully updated'
         ]);
     }
 
