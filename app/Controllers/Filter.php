233c59e2b3182a53252bdd3b5a5f20c891c73217
dancerking <dancerking306@gmail.com>
2022-11-21 16:29:48 +0100

    completed filter endpoint

--- app/Controllers/Filter.php
+++ app/Controllers/Filter.php
@@ -5,13 +5,14 @@ namespace App\Controllers;
 use App\Controllers\APIBaseController;
 use App\Models\FilterMappingModel;
 use App\Models\FilterModel;
+use App\Models\TypeMappingModel;
 use CodeIgniter\API\ResponseTrait;
 
 class Filter extends APIBaseController
 {
     /**
-     * Return an array of Filter mapping
-     * GET/filters
+     * Insert Filter mapping
+     * POST/filters
      * @return mixed
      */
     use ResponseTrait;
@@ -20,6 +21,7 @@ class Filter extends APIBaseController
         /* Load Rate Model */
         $filter_mapping_model = new FilterMappingModel();
         $filter_model = new FilterModel();
+        $type_mapping_model = new TypeMappingModel();
 
         /* Getting host_id from JWT token */
         $host_id = $this->get_host_id();
@@ -29,6 +31,7 @@ class Filter extends APIBaseController
             !$this->validate([
                 'filter_mapping_level' => 'required',
                 'filter_mapping_type' => 'required',
+                'filter_code' => 'required',
             ])
         ) {
             return $this->notifyError(
@@ -45,7 +48,9 @@ class Filter extends APIBaseController
         $filter_mapping_type = $this->request->getVar(
             'filter_mapping_type'
         );
-
+        $filter_code = $this->request->getVar(
+            'filter_code'
+        );
         if (
             $filter_mapping_level != 1 &&
             $filter_mapping_level != 2
@@ -56,19 +61,70 @@ class Filter extends APIBaseController
                 'filter'
             );
         }
-
-        /* Getting filter mapping ids from FilterMapping Model */
-        $filter_mapping_ids = $filter_mapping_model->get_available_ids(
-            $host_id,
-            $filter_mapping_type,
-            $filter_mapping_level
-        );
+        $filter_codes = $filter_model
+            ->where([
+                'filter_level' => $filter_mapping_level,
+            ])
+            ->select('filter_code')
+            ->findAll();
+        $filter_codes_array = [];
+        foreach ($filter_codes as $code) {
+            array_push(
+                $filter_codes_array,
+                $code['filter_code']
+            );
+        }
+        if (!in_array($filter_code, $filter_codes_array)) {
+            return $this->notifyError(
+                'Filter code is invalid code.',
+                'invalid_data',
+                'filter'
+            );
+        }
+        /* Insert filter mapping ids in FilterMapping Model */
+        if (
+            $filter_mapping_level == 1 &&
+            $filter_mapping_type != 0
+        ) {
+            return $this->notifyError(
+                'When filter_mapping_level is 1, filter_mapping_type must be 0.',
+                'invalid_data',
+                'filter'
+            );
+        }
+        if ($filter_mapping_level == 2) {
+            if (
+                $type_mapping_model
+                    ->where([
+                        'type_mapping_code' => $filter_mapping_type,
+                    ])
+                    ->findAll() == null
+            ) {
+                return $this->notifyError(
+                    'No Such type_mapping_code',
+                    'invalid_data',
+                    'filter'
+                );
+            }
+        }
+        $new_id = $filter_mapping_model->insert([
+            'filter_mapping_host_id' => $host_id,
+            'filter_mapping_code' => $filter_code,
+            'filter_mapping_level' => $filter_mapping_level,
+            'filter_mapping_type' => $filter_mapping_type,
+            'filter_mapping_status' => 1,
+        ]);
+        if (!$new_id) {
+            return $this->notifyError(
+                'Failed create',
+                'failed_create',
+                'filter'
+            );
+        }
 
         return $this->respond([
-            'filters_mapping_id' =>
-                $filter_mapping_ids == null
-                    ? []
-                    : $filter_mapping_ids,
+            'filters_mapping_id' => $new_id,
+            'message' => 'Successfully created.',
         ]);
     }
 
