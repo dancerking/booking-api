cd000bff7421f89c2315143c1cf39b7008f270e4
dancerking <dancerking306@gmail.com>
2022-11-22 03:36:23 +0100

    added booking endpoint

---
+++ app/Controllers/Booking.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\APIBaseController;
+use App\Models\HostBookingModel;
+use CodeIgniter\API\ResponseTrait;
+use DateTime;
+
+class Booking extends APIBaseController
+{
+    /**
+     * Return an array of Booking
+     * GET/bookings
+     * @return mixed
+     */
+    use ResponseTrait;
+    public function index()
+    {
+        $config = config('Config\App');
+
+        /* Load HostBooking Model */
+        $host_booking_model = new HostBookingModel();
+
+        /* Getting host_id from JWT token */
+        $host_id = $this->get_host_id();
+
+        /* Validate */
+        $rules = [
+            'bookingFrom' => 'required',
+            'bookingTo' => 'required',
+        ];
+        if (!$this->validate($rules)) {
+            return $this->notifyError(
+                'Input data format is incorrect',
+                'invalid_data',
+                'booking'
+            );
+        }
+
+        /* Getting service calendar from ServicePriceCalendarModel */
+        // today > bookingFrom, today-bookingFrom <= 90
+        $bookingfrom = $this->request->getVar(
+            'bookingFrom'
+        );
+        // bookingTo > bookingFrom, bookingTo-bookingFrom <= 90,
+        $bookingto = $this->request->getVar('bookingTo');
+
+        // check if date format
+        if (
+            !$this->validateDate($bookingfrom) ||
+            !$this->validateDate($bookingto)
+        ) {
+            return $this->notifyError(
+                'Date format is incorrect',
+                'invalid_data',
+                'booking'
+            );
+        }
+        if (
+            new DateTime($bookingto) <
+            new DateTime($bookingfrom)
+        ) {
+            return $this->notifyError(
+                'To date should be larger than From date.',
+                'invalid_data',
+                'booking'
+            );
+        }
+        if (
+            date_diff(
+                new DateTime($bookingto),
+                new DateTime($bookingfrom)
+            )->days > $config->maximum_date_range
+        ) {
+            return $this->notifyError(
+                'date range is maximum ' .
+                    $config->maximum_date_range .
+                    ' days',
+                'invalid_data',
+                'booking'
+            );
+        }
+        if (new DateTime($bookingfrom) > new DateTime()) {
+            return $this->notifyError(
+                'from date should be smaller than today date.',
+                'invalid_data',
+                'booking'
+            );
+        }
+        if (
+            date_diff(
+                new DateTime(),
+                new DateTime($bookingfrom)
+            )->days > $config->maximum_date_range
+        ) {
+            return $this->notifyError(
+                'date range is maximum ' .
+                    $config->maximum_date_range .
+                    ' days',
+                'invalid_data',
+                'booking'
+            );
+        }
+        // Getting availabile data from model
+        $booking = $host_booking_model->get_bookings(
+            $host_id,
+            $bookingfrom,
+            $bookingto
+        );
+
+        return $this->respond([
+            'booking' => $booking == null ? [] : $booking,
+        ]);
+    }
+
+    public function validateDate($date, $format = 'Y-m-d')
+    {
+        $d = DateTime::createFromFormat($format, $date);
+        return $d && $d->format($format) === $date;
+    }
+}
