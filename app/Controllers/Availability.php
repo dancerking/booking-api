af4059d8fa79640632bc2fbfc7ac69ba313f13bc
dancerking <dancerking306@gmail.com>
2022-11-28 14:04:27 +0100

    updated availability/update endpoint

--- app/Controllers/Availability.php
+++ app/Controllers/Availability.php
@@ -6,8 +6,6 @@ use App\Controllers\APIBaseController;
 use App\Models\TypeAvailabilityModel;
 use App\Models\TypeMappingModel;
 use CodeIgniter\API\ResponseTrait;
-use DateInterval;
-use DatePeriod;
 use DateTime;
 
 class Availability extends APIBaseController
@@ -130,12 +128,7 @@ class Availability extends APIBaseController
 
         /* Validate */
         $rules = [
-            'type_availability_code' => 'required',
-            'type_availability_day' => 'required',
-            'type_availability_qty' => 'required',
-            'type_availability_msa' => 'required',
-            'type_availability_coa' => 'required',
-            'type_availability_cod' => 'required',
+            'type_code' => 'required',
         ];
         if (!$this->validate($rules)) {
             return $this->notifyError(
@@ -146,202 +139,279 @@ class Availability extends APIBaseController
         }
 
         /* Getting request data */
-        $type_availability_code = $this->request->getVar(
-            'type_availability_code'
-        );
-        $type_availability_day = $this->request->getVar(
-            'type_availability_day'
-        );
-        $type_availability_qty = $this->request->getVar(
-            'type_availability_qty'
-        );
-        $type_availability_msa = $this->request->getVar(
-            'type_availability_msa'
-        );
-        $type_availability_coa = $this->request->getVar(
-            'type_availability_coa'
-        );
-        $type_availability_cod = $this->request->getVar(
-            'type_availability_cod'
-        );
-
+        $type_code = $this->request->getVar('type_code');
+        if (!is_array($type_code)) {
+            return $this->notifyError(
+                'type_code should be array',
+                'invalid_data',
+                'availability'
+            );
+        }
+        if (
+            count($type_code) > $config->MAXIMUM_DATE_RANGE
+        ) {
+            return $this->notifyError(
+                'Max rows are ' .
+                    $config->MAXIMUM_DATE_RANGE,
+                'overflow',
+                'availability'
+            );
+        }
         /* Format validation */
-        if (
-            $type_mapping_model
-                ->where([
-                    'type_mapping_host_id' => $host_id,
-                    'type_mapping_code' => $type_availability_code,
-                ])
-                ->findAll() == null
-        ) {
-            return $this->notifyError(
-                'type_mapping_code is invalid',
-                'notFound',
-                'availability'
-            );
-        }
-        if (
-            !isset($type_availability_day->from) ||
-            !isset($type_availability_day->to)
-        ) {
-            return $this->notifyError(
-                'type_availability_day must have `from` and `to` fields',
-                'invalid_data',
-                'availability'
-            );
-        }
-        if (
-            !$this->validateDate(
-                $type_availability_day->from
-            ) ||
-            !$this->validateDate($type_availability_day->to)
-        ) {
-            return $this->notifyError(
-                'Date format is incorrect',
-                'invalid_data',
-                'availability'
-            );
-        }
-        if (
-            new DateTime() >
-                new DateTime(
-                    $type_availability_day->from
+        foreach ($type_code as $row) {
+            if (!isset($row->type_availability_code)) {
+                return $this->notifyError(
+                    'type_availability_code is required',
+                    'invalid_data',
+                    'availability'
+                );
+            }
+
+            if (!isset($row->type_availability_from)) {
+                return $this->notifyError(
+                    'type_availability_from is required',
+                    'invalid_data',
+                    'availability'
+                );
+            }
+
+            if (!isset($row->type_availability_to)) {
+                return $this->notifyError(
+                    'type_availability_to is required',
+                    'invalid_data',
+                    'availability'
+                );
+            }
+
+            if (!isset($row->type_availability_qty)) {
+                return $this->notifyError(
+                    'type_availability_qty is required',
+                    'invalid_data',
+                    'availability'
+                );
+            }
+
+            if (!isset($row->type_availability_msa)) {
+                return $this->notifyError(
+                    'type_availability_msa is required',
+                    'invalid_data',
+                    'availability'
+                );
+            }
+
+            if (!isset($row->type_availability_coa)) {
+                return $this->notifyError(
+                    'type_availability_coa is required',
+                    'invalid_data',
+                    'availability'
+                );
+            }
+
+            if (!isset($row->type_availability_cod)) {
+                return $this->notifyError(
+                    'type_availability_cod is required',
+                    'invalid_data',
+                    'availability'
+                );
+            }
+            if (
+                $type_mapping_model
+                    ->where([
+                        'type_mapping_host_id' => $host_id,
+                        'type_mapping_code' =>
+                            $row->type_availability_code,
+                    ])
+                    ->findAll() == null
+            ) {
+                return $this->notifyError(
+                    'type_mapping_code is invalid',
+                    'notFound',
+                    'availability'
+                );
+            }
+            if (
+                !$this->validateDate(
+                    $row->type_availability_from
                 ) ||
-            new DateTime() >
-                new DateTime($type_availability_day->to)
-        ) {
-            return $this->notifyError(
-                'type_availability_day should be larger than today',
-                'invalid_data',
-                'availability'
-            );
+                !$this->validateDate(
+                    $row->type_availability_to
+                )
+            ) {
+                return $this->notifyError(
+                    'Date format is incorrect',
+                    'invalid_data',
+                    'availability'
+                );
+            }
+            if (
+                new DateTime() >
+                new DateTime($row->type_availability_from)
+            ) {
+                return $this->notifyError(
+                    'type_availability_from should be larger than today',
+                    'invalid_data',
+                    'availability'
+                );
+            }
+            if (
+                new DateTime($row->type_availability_to) <
+                new DateTime($row->type_availability_from)
+            ) {
+                return $this->notifyError(
+                    'type_availability_to should be larger than type_availability_from.',
+                    'invalid_data',
+                    'availability'
+                );
+            }
+            if (
+                date_diff(
+                    new DateTime(
+                        $row->type_availability_to
+                    ),
+                    new DateTime(
+                        $row->type_availability_from
+                    )
+                )->days > $config->MAXIMUM_DATE_RANGE
+            ) {
+                return $this->notifyError(
+                    'date range is maximum ' .
+                        $config->MAXIMUM_DATE_RANGE .
+                        ' days',
+                    'invalid_data',
+                    'availability'
+                );
+            }
+            if (
+                !ctype_digit(
+                    (string) $row->type_availability_qty
+                )
+            ) {
+                return $this->notifyError(
+                    'Type availability qty format is incorrect',
+                    'invalid_data',
+                    'availability'
+                );
+            }
+            if (
+                !ctype_digit(
+                    (string) $row->type_availability_msa
+                )
+            ) {
+                return $this->notifyError(
+                    'Type availability msa format is incorrect',
+                    'invalid_data',
+                    'availability'
+                );
+            }
+            if (
+                !ctype_digit(
+                    (string) $row->type_availability_coa
+                )
+            ) {
+                return $this->notifyError(
+                    'Type availability coa format is incorrect',
+                    'invalid_data',
+                    'availability'
+                );
+            }
+            if (
+                $row->type_availability_coa < 0 ||
+                $row->type_availability_coa > 1
+            ) {
+                return $this->notifyError(
+                    'type_availability_coa should be 0 or 1',
+                    'invalid_data',
+                    'availability'
+                );
+            }
+            if (
+                !ctype_digit(
+                    (string) $row->type_availability_cod
+                )
+            ) {
+                return $this->notifyError(
+                    'Type availability cod format is incorrect',
+                    'invalid_data',
+                    'availability'
+                );
+            }
+            if (
+                $row->type_availability_cod < 0 ||
+                $row->type_availability_cod > 1
+            ) {
+                return $this->notifyError(
+                    'type_availability_cod should be 0 or 1',
+                    'invalid_data',
+                    'availability'
+                );
+            }
         }
-        if (
-            new DateTime($type_availability_day->to) <
-            new DateTime($type_availability_day->from)
-        ) {
-            return $this->notifyError(
-                'To date should be larger than From date.',
-                'invalid_data',
-                'availability'
-            );
-        }
-        if (
-            date_diff(
-                new DateTime($type_availability_day->to),
-                new DateTime($type_availability_day->from)
-            )->days > $config->MAXIMUM_DATE_RANGE
-        ) {
-            return $this->notifyError(
-                'date range is maximum ' .
-                    $config->MAXIMUM_DATE_RANGE .
-                    ' days',
-                'invalid_data',
-                'availability'
-            );
-        }
-        if (!ctype_digit((string) $type_availability_qty)) {
-            return $this->notifyError(
-                'Type availability qty format is incorrect',
-                'invalid_data',
-                'availability'
-            );
-        }
-        if (!ctype_digit((string) $type_availability_msa)) {
-            return $this->notifyError(
-                'Type availability msa format is incorrect',
-                'invalid_data',
-                'availability'
-            );
-        }
-        if (!ctype_digit((string) $type_availability_coa)) {
-            return $this->notifyError(
-                'Type availability coa format is incorrect',
-                'invalid_data',
-                'availability'
-            );
-        }
-        if (
-            $type_availability_coa < 0 ||
-            $type_availability_coa > 1
-        ) {
-            return $this->notifyError(
-                'type_availability_coa should be 0 or 1',
-                'invalid_data',
-                'availability'
-            );
-        }
-        if (!ctype_digit((string) $type_availability_cod)) {
-            return $this->notifyError(
-                'Type availability cod format is incorrect',
-                'invalid_data',
-                'availability'
-            );
-        }
-        if (
-            $type_availability_cod < 0 ||
-            $type_availability_cod > 1
-        ) {
-            return $this->notifyError(
-                'type_availability_cod should be 0 or 1',
-                'invalid_data',
-                'availability'
-            );
-        }
+
         /* Update data in DB */
-        $from = $type_availability_day->from;
-        $to = $type_availability_day->to;
-        while (strtotime($from) <= strtotime($to)) {
-            $data = [
-                'type_availability_host_id' => $host_id,
-                'type_availability_code' => $type_availability_code,
-                'type_availability_day' => $from,
-                'type_availability_qty' => $type_availability_qty,
-                'type_availability_msa' => $type_availability_msa,
-                'type_availability_coa' => $type_availability_coa,
-                'type_availability_cod' => $type_availability_cod,
-            ];
-            $matched_ids = $type_availability_model
-                ->where([
+        foreach ($type_code as $row) {
+            $from = $row->type_availability_from;
+            $to = $row->type_availability_to;
+            while (strtotime($from) <= strtotime($to)) {
+                $data = [
                     'type_availability_host_id' => $host_id,
-                    'type_availability_code' => $type_availability_code,
-                    'type_availability_day' =>
-                        $data['type_availability_day'],
-                ])
-                ->findAll();
-            if ($matched_ids != null) {
-                foreach ($matched_ids as $matched_id) {
+                    'type_availability_code' =>
+                        $row->type_availability_code,
+                    'type_availability_day' => $from,
+                    'type_availability_qty' =>
+                        $row->type_availability_qty,
+                    'type_availability_msa' =>
+                        $row->type_availability_msa,
+                    'type_availability_coa' =>
+                        $row->type_availability_coa,
+                    'type_availability_cod' =>
+                        $row->type_availability_cod,
+                ];
+                $matched_ids = $type_availability_model
+                    ->where([
+                        'type_availability_host_id' => $host_id,
+                        'type_availability_code' =>
+                            $data['type_availability_code'],
+                        'type_availability_day' =>
+                            $data['type_availability_day'],
+                    ])
+                    ->findAll();
+                if ($matched_ids != null) {
+                    foreach ($matched_ids as $matched_id) {
+                        if (
+                            !$type_availability_model->update(
+                                $matched_id[
+                                    'type_availability_id'
+                                ],
+                                $data
+                            )
+                        ) {
+                            return $this->notifyError(
+                                'Failed update',
+                                'failed_update',
+                                'availability'
+                            );
+                        }
+                    }
+                } else {
                     if (
-                        !$type_availability_model->update(
-                            $matched_id[
-                                'type_availability_id'
-                            ],
+                        !$type_availability_model->insert(
                             $data
                         )
                     ) {
                         return $this->notifyError(
-                            'Failed update',
-                            'failed_update',
+                            'Failed insert',
+                            'failed_insert',
                             'availability'
                         );
                     }
                 }
-            } else {
-                if (
-                    !$type_availability_model->insert($data)
-                ) {
-                    return $this->notifyError(
-                        'Failed insert',
-                        'failed_insert',
-                        'availability'
-                    );
-                }
+                $from = date(
+                    'Y-m-d',
+                    strtotime('+1 day', strtotime($from))
+                );
             }
-            $from = date(
-                'Y-m-d',
-                strtotime('+1 day', strtotime($from))
-            );
         }
+
         return $this->respond([
             'message' => 'Successfully updated',
         ]);
