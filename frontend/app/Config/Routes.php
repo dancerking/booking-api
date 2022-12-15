ce25b16558e147e6c374667a93b9b7e6d60fdc71
dancerking <dancerking306@gmail.com>
2022-12-15 17:15:36 +0100

    add: filters & routes for `login`

--- frontend/app/Config/Routes.php
+++ frontend/app/Config/Routes.php
@@ -36,6 +36,9 @@ $routes->set404Override();
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
+$routes->get('/login', 'AuthController::index');
+$routes->post('/login', 'AuthController::login');
+$routes->get('/logout', 'AuthController::logout');
 
 /*
  * --------------------------------------------------------------------
